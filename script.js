2b76653e7080b17adcc4358c23ec1f9b62513934
Alvaro Souza <40568904+alvarosouza01@users.noreply.github.com>
2024-01-23 08:52:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,12 +20,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     showLanguageContent(primaryLanguage);
 
-    document.addEventListener('click', function (event) {
-        var target = event.target;
-
-        if (target.tagName === 'A' || target.tagName === 'STRONG') {
+    function handleLanguageClick(event) {
+        var target = event.target.closest('[data-lang]');
+        if (target) {
             var selectedLang = target.getAttribute('data-lang');
             showLanguageContent(selectedLang);
         }
-    });
+    }
+
+    document.body.addEventListener('click', handleLanguageClick);
+    document.body.addEventListener('touchend', handleLanguageClick);
 });
+function as()
+{
+    console.log("Clikced");
+}
